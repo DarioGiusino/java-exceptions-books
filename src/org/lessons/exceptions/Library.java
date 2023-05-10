0a0bbb55b9a5d5f3ca9a85a04725df911c7e71ac
DarioGiusino <dario.giusino@gmail.com>
2023-05-10 15:35:25 +0200

    writing and reading text file

--- src/org/lessons/exceptions/Library.java
+++ src/org/lessons/exceptions/Library.java
@@ -1,12 +1,14 @@
 package org.lessons.exceptions;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Library {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		
+
 		int index = 0;
 
 		System.out.println("Benvenuto, quanti libri vuoi creare?");
@@ -14,33 +16,54 @@ public class Library {
 
 		Book[] books = new Book[number];
 
-		while(index < number) {
+		while (index < number) {
 			System.out.println("------------Nuovo Libro----------------");
 			System.out.print("Inserisci il titolo del libro: ");
-			String title = sc.nextLine();
-
-			sc.nextLine();
+			String title = sc.next();
 
 			System.out.print("Inserisci il numero di pagine che contiene: ");
 			int pages = sc.nextInt();
 
 			System.out.print("Inserisci l'autore del libro: ");
-			String author = sc.nextLine();
-
-			sc.nextLine();
+			String author = sc.next();
 
 			System.out.print("Inserisci l'editore del libro: ");
-			String editor = sc.nextLine();
+			String editor = sc.next();
 
 			try {
 				System.out.println("---------------------------------------\n");
 				Book newBook = new Book(title, pages, author, editor);
 				books[index++] = newBook;
 			} catch (Exception e) {
-				System.out.println("An error occured: " + e.getMessage() +"\nRitenta con un nuovo inserimeto...\n");
+				System.out.println("An error occured: " + e.getMessage() + "\nRitenta con un nuovo inserimeto...\n");
 				continue;
 			}
 		}
 		sc.close();
+
+		for(int x = 0; x < books.length; x++) {
+			try {
+				FileWriter myWriter = new FileWriter("C:/Users/dario/Desktop/nuovo.txt", true);
+				myWriter.write(books[x].toString() + "\n\n");
+				myWriter.close();				
+			} catch (Exception e) {
+				System.out.println("Error creating file: " + e.getMessage());
+			}
+		}
+		
+		File textFile = new File("C:/Users/dario/Desktop/nuovo.txt");
+		
+		try {
+			Scanner reader = new Scanner(textFile);
+			System.out.println("\n----Libreria----\n");
+			while(reader.hasNextLine()) {
+				System.out.println(reader.nextLine());
+			}
+			reader.close();
+		} catch (Exception e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+		
+		
 	}
 }
